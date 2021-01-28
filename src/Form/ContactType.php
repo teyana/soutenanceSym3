1a5e616f2569ce913129cdaa17f653bb06d1d906
nvnouush <moumile.jihane@gmail.com>
2021-01-28 15:49:14 +0100

    Update ContactType.php

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -6,17 +6,37 @@ use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
+    /**
+     * Permet d'avoir la configuration d'un champ
+     *
+     * @param [type] $label
+     * @param [type] $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder){
+        return[
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ];
+
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('phone')
-            ->add('mail')
-            ->add('message')
+            ->add('firstName', TextType::class, $this->getConfiguration("Prénom", "Votre prénom"))
+            ->add('lastName', TextType::class, $this->getConfiguration("Nom", "Votre nom"))
+            ->add('phone', IntegerType::class, $this->getConfiguration("Numéro de téléphone", "Votre numéro de téléphone"))
+            ->add('mail', TextType::class, $this->getConfiguration("Adresse mail", "Votre adresse mail"))
+            ->add('message', TextareaType::class, $this->getConfiguration("Message", "Ecrivez-nous votre message ici"))
         ;
     }
 
