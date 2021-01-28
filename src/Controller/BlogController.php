189397c41040b8b061e71a20a7a11f41beb87f95
Ahoni <asfaenti.zoubert@hotmail.com>
2021-01-28 23:57:04 +0100

    modification blogcontroller version ArticleRepository

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,8 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo)
     {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-
         $articles = $repo->findAll();
 
         return $this->render('blog/index.html.twig', [
@@ -27,10 +26,8 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show($id)
+    public function show(ArticleRepository $repo, $id)
     {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-
         $article = $repo->find($id);
 
         return $this->render('blog/show.html.twig', [
