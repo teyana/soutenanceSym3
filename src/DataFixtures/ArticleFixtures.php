cb08505ff5e931afe53afdea894d4b21d977efe7
Ahoni <asfaenti.zoubert@hotmail.com>
2021-02-01 18:43:44 +0100

    ajout creation $faker + faker title / content / slug

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,18 +5,21 @@ namespace App\DataFixtures;
 use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('fr_FR');
+
         for ($i = 1; $i <= 20; $i++) {
             $article = new Article();
-            $article->setTitle("Titre de l'article n$i")
-                ->setContent("<p>Contenu de l'article n$i . Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque vitae laudantium autem tenetur repellendus iusto quis quo aliquam ipsam explicabo ipsum quod inventore velit tempore facere ea ad sequi itaque vero, doloribus temporibus ut. Ipsum rem vero asperiores eveniet, alias mollitia maxime delectus voluptas necessitatibus id iste deleniti blanditiis iure.</p>")
+            $article->setTitle($faker->sentence())
+                ->setContent($faker->text(mt_rand(500, 750)))
                 ->setImage("http://placehold.it/350x150")
                 ->setCreatedAt(new \DateTime())
-                ->setSlug("article-n-$i");
+                ->setSlug($faker->slug());
 
             $manager->persist($article);
         }
