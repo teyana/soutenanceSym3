c86bc246dc3925f3c3e083f41a69a7624de7bdb5
Sissoko Aïssata <aissata001@hotmail.fr>
2021-02-02 17:15:48 +0100

    Revert "Revert "erreur""
    This reverts commit 92697392705cf6da6a3899cc4202a8d90ea565a1.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,10 +13,6 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'title_vente' => "Meilleur vente",
-            'title_histoire' => "L'histoire / Le concept"
-        ]);
+        return $this->render('home/index.html.twig');
     }
-
 }
