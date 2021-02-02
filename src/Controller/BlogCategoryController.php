caf515a1c40d1ca52f11951085e977f143ce6520
Ahoni <asfaenti.zoubert@hotmail.com>
2021-02-02 15:31:39 +0100

    crete form creation category

--- src/Controller/BlogCategoryController.php
+++ src/Controller/BlogCategoryController.php
@@ -2,20 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogCategory;
+use App\Form\BlogCategoryType;
 use App\Repository\BlogCategoryRepository;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class BlogCategoryController extends AbstractController
 {
     /**
      * @Route("/admin/blogCategory/create",name="blogCategory_create")
      */
-    public function create()
+    public function create(Request $request, EntityManagerInterface $em, SluggerInterface $slugger)
     {
-        return $this->render('blog_category/create.html.twig');
+        $blogCategory = new BlogCategory;
+
+        $form = $this->createForm(BlogCategoryType::class, $blogCategory);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $blogCategory->setSlug(strtolower($slugger->slug($blogCategory->getName())));
+            $em->persist($blogCategory);
+            $em->flush();
+
+            return $this->redirectToRoute('blog', []);
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('blog_category/create.html.twig', [
+            'formView' => $formView
+        ]);
     }
 
     /**
